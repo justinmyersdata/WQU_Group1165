deabab7d1c31641c9dcedaa4af9e585602e4caca
Justin Myers <justinmyersdata@gmail.com>
2023-02-11 15:29:38 -0600

    updates to files and functions

---
+++ functions/pandas_funcs.py
@@ -0,0 +1,9 @@
+
+
+def buy_or_sell(row,size,band):
+    if row[f'{size}_day_ma_return'] + band*row[f'{size}_day_ma_return'] < row['daily_return']:
+        return 1
+    elif row[f'{size}_day_ma_return'] - band*row[f'{size}_day_ma_return'] > row['daily_return']:
+        return -1
+    else:
+        return 0
